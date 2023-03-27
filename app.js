5e14ee108ad768788eafc680a25731b6ed447bec
enchanted-coder <danieltogey@pm.me>
2023-03-28 02:56:20 +0300

    final pong before i sleep

--- app.js
+++ app.js
@@ -1,132 +1,134 @@
-const canvas = document.querySelector("canvas")
-const c = canvas.getContext("2d")
+const canvas = document.getElementById("myPongGame")
+const ctx = canvas.getContext("2d")
 
-canvas.width = innerWidth
-canvas.height = innerHeight
+const ball = {
+  x: canvas.width / 2,
+  y: canvas.height / 2, 
+  radius: 10,
+  speed: 5,
+  dx: 5,
+  dy: 5,
+};
 
-class Paddle{
-    constructor({position}){
-        this.position = position
-        this.velocity = {
-            x: 0,
-            y: 0
-        }
-        this.width = 10
-        this.height = 100
-    }
-
-    draw(){
-        c.fillStyle = "white"
-
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)
-    }
-    update(){
-        this.draw()
-
-        if(this.position.y + this.velocity.y > 0 && this.position.y + this.height + this.velocity.y < canvas.height)
-            this.position.y += this.velocity.y
-    }
+const leftPaddle = {
+  x: 0,
+  y: canvas.height / 2 - 50,
+  width: 10,
+  height: 100,
+  speed: 10,
+  dy: 0,
 }
 
-class Ball{
-    constructor({position}){
-        this.position = position
-        
-        const speed = 2
-        const direction = {
-            x: Math.random() - 0.5 >= 0 ? -speed : speed,
-            y: Math.random() - 0.5 >= 0 ? -speed : speed,
-        }
-        this.velocity = {
-            x: direction.x,
-            y: direction.y,
-        }
-        this.width = 10
-        this.height = 10
-    }
-    draw(){
-        c.fillStyle = 'white'
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)
-       
-    }
-    update(){
-        this.draw()
-
-        const rightSide = this.position.x + this.width + this.velocity.x
-        const leftSide = this.position.x + this.velocity.x
-        const bottomSide = this.position.y + this.height
-        const topSide = this.position.y
-
-        // paddle 1
-        if((leftSide >= paddle1.position.x + paddle1.width &&  bottomSide >= paddle1.position.y && topSide <= paddle1.position.y + paddle1.height)){
-            this.velocity.x = -this.velocity.x
-        }
-
-        // paddle 2
-        if((rightSide >= paddle2.position.x &&  bottomSide >= paddle2.position.y && topSide <= paddle2.position.y + paddle2.height)){
-            this.velocity.x = -this.velocity.x
-        }
-
-        if(this.position.y + this.height + this.velocity.y >= canvas.height || this.position.y <= 0){
-            this.velocity.y = -this.velocity.y
-        }
-
-        
-
-        this.position.x += this.velocity.x
-        this.position.y += this.velocity.y
-    }
+const rightPaddle = {
+  x: canvas.width - 10,
+  y: canvas.height / 2 - 50,
+  width: 10,
+  height: 100,
+  speed: 5,
+  dy: 0,
 }
 
-const paddle1 = new Paddle({position: {
-    x: 10,
-    y: 100,
-  },
-})
 
-const paddle2 = new Paddle({position: {
-    x: canvas.width - 10 * 2,
-    y: 100,
-  },
-})
+function draw(){
+  // clears the canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
 
-const ball = new Ball({
-    position: {
-        x: canvas.width / 2,
-        y: canvas.height / 2,
-    },
-})
+  // drawing the ball
+  ctx.beginPath()
+  ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2)
+  ctx.fill()
+  ctx.closePath()
+
+  // drawing left paddle
+  ctx.beginPath()
+  ctx.rect(leftPaddle.x, leftPaddle.y, leftPaddle.width, leftPaddle.height)
+  ctx.fill()
+  ctx.closePath()
+
+  //drawing the right paddle
+  ctx.beginPath()
+  ctx.rect(rightPaddle.x, rightPaddle.y, rightPaddle.width, rightPaddle.height)
+  ctx.fill()
+  ctx.closePath()
 
 
-
-function animate(){
-    requestAnimationFrame(animate)
-    c.fillStyle = 'black'
-    c.fillRect(0, 0, canvas.width, canvas.height)
-    paddle1.update()
-    paddle2.update()
-
-    ball.update()
 }
 
-animate()
+// moving the ball and paddles
+  function update(){
+  //the ball
+  ball.x += ball.dx
+  ball.y += ball.dy 
 
-addEventListener('keydown', (event) => {
-    const speed = 4
+  // ball collision with walls
+  if(ball.y + ball.radius > canvas.height || ball.y - ball.radius < 0){
+    ball.dy = -ball.dy
+  }
 
-    switch (event.key) {
-        case 'w':
-            paddle1.velocity.y = -speed
-            break
-        case 's':
-            paddle1.velocity.y = speed
-            break
-        case 'ArrowUp':
-            paddle2.velocity.y = -speed
-            break
-        case 'ArrowDown':
-            paddle2.velocity.y = speed
-            break
+  //ball collision with left paddle 
+  if(ball.x - ball.radius < leftPaddle.x + leftPaddle.width && ball.y > leftPaddle.y && ball.y < leftPaddle.y + leftPaddle.height){
+    ball.dx = -ball.dx
+  }
 
-    }
-})
+  //ball collision with right paddle 
+  if(ball.x + ball.radius > rightPaddle.x && ball.y > rightPaddle.y && ball.y < rightPaddle.y + rightPaddle.height){
+    ball.dx = -ball.dx
+  }
+
+  // move the left paddle 
+  leftPaddle.y += leftPaddle.dy 
+
+  // contain the left paddle within canvas 
+  if(leftPaddle.y + leftPaddle.height > canvas.height){
+    leftPaddle.y = canvas.height - leftPaddle.height
+  }else if(leftPaddle.y < 0) {
+    leftPaddle.y = 0
+  }
+
+  // Computer aided paddle 
+  // Computer is just following the ball basically
+
+  rightPaddle.y += (ball.y - (rightPaddle.y + rightPaddle.height / 2)) * rightPaddle.speed / 100
+
+  // contain right paddle withing canvas
+  if(rightPaddle.y + rightPaddle.height > canvas.height){
+    rightPaddle.y = canvas.height - rightPaddle.height 
+  } else if(rightPaddle.y < 0){
+    rightPaddle.y = 0
+  }
+}
+
+// eventlisteners for the player for left paddle
+function controlLeftPaddle(event){
+  switch(event.keyCode){
+    case 38: // 38 means up arrow
+      leftPaddle.dy = -leftPaddle.speed
+      break 
+    case 40: // 40 means down arrow 
+      leftPaddle.dy = leftPaddle.speed
+      break
+  }
+}
+
+function stopLeftPaddle(event){
+  switch(event.keyCode){
+    case 38:
+    case 40:
+      leftPaddle.dy = 0 
+      break
+  }
+}
+
+// start the game 
+function start(){
+  setInterval(function () {
+    update()
+    draw()
+  }, 10)
+
+  document.addEventListener("keydown", controlLeftPaddle)
+  document.addEventListener("keyup", stopLeftPaddle)
+}
+
+start()
+
